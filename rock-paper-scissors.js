ccd8523d5689d541e2222dd688467b54a4bb983a
m-radman <milrad@live.com>
2022-11-21 12:05:13 +0100

    Added style

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,55 +1,76 @@
-let computerSelection = getComputerChoice();
+const ALL_CHOICES = ["rock", "paper", "scissors"];
 const choices = document.querySelectorAll("button");
 const start = document.getElementById("start");
 start.addEventListener("click", () => game());
-let playerWin = 0;
-let computerWin = 0;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
 
-document.getElementById("rock").disabled = true;
-document.getElementById("paper").disabled = true;
-document.getElementById("scissors").disabled = true;
+const disableControls = () => {
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
+}
 
-document.getElementById("new-game").addEventListener("click", () => {
-        playerWin = 0, computerWin = 0
-        document.getElementById("computer-score").textContent = `Computer: ${computerWin}`;
-        document.getElementById("player-score").textContent = `Player :${playerWin}`;
-        document.getElementById("rock").disabled = false;
-        document.getElementById("paper").disabled = false;
-        document.getElementById("scissors").disabled = false;
-    });
+const enableControls = () => {
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+}
+
+function resetGame() {
+    playerWinCounter = 0, computerWinCounter = 0
+    document.getElementById("computer-score").textContent = `Computer: ${computerWinCounter}`;
+    document.getElementById("player-score").textContent = `Player :${playerWinCounter}`;
+    enableControls();
+}
+
+disableControls();
+
+document.getElementById("new-game").addEventListener("click", () => resetGame());
 
 function getComputerChoice() {
-    let compChoice = ["rock", "paper", "scissors"];
-    const choiceIndex = Math.floor(Math.random() * compChoice.length);
-    return compChoice[choiceIndex];
+    const choiceIndex = Math.floor(Math.random() * ALL_CHOICES.length);
+    return ALL_CHOICES[choiceIndex];
 };
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    // todo: refactor if else mess, replace with switch? 
+    // switch (computerSelection) {
+    //     case "rock":
+
+    //     case "paper":
+
+    //     case "scissors":
+
+    //     default:
+    //         throw new Error("Invalid computer selection!");
+    // };
 
     if (computerSelection == "rock" && playerSelection == "scissors") {
-        document.getElementById("info").textContent =("You lose!");
-       return document.getElementById("computer-score").textContent = `Computer: ${++computerWin}`;
+        document.getElementById("info").textContent = ("Scissors lose to rock!");
+        return document.getElementById("computer-score").textContent = `Computer: ${++computerWinCounter}`;
     }
     else if (computerSelection == "rock" && playerSelection == "paper") {
-        document.getElementById("info").textContent = ("You win!");
-        return document.getElementById("player-score").textContent = `Player :${++playerWin}`;
+        document.getElementById("info").textContent = ("Paper beats rock!");
+        return document.getElementById("player-score").textContent = `Player: ${++playerWinCounter}`;
     }
     else if (computerSelection == "paper" && playerSelection == "rock") {
-        document.getElementById("info").textContent = ("You lose!");
-        return document.getElementById("computer-score").textContent = `Computer: ${++computerWin}`;
+        document.getElementById("info").textContent = ("Rock lose to paper!");
+        return document.getElementById("computer-score").textContent = `Computer: ${++computerWinCounter}`;
     }
     else if (computerSelection == "paper" && playerSelection == "scissors") {
-        document.getElementById("info").textContent = ("You win!");
-        return document.getElementById("player-score").textContent = `Player :${++playerWin}`;
+        document.getElementById("info").textContent = ("Scissors beats paper!");
+        return document.getElementById("player-score").textContent = `Player: ${++playerWinCounter}`;
     }
     else if (computerSelection == "scissors" && playerSelection == "rock") {
-        document.getElementById("info").textContent = ("You win!");
-        return document.getElementById("player-score").textContent = `Player :${++playerWin}`;
+        document.getElementById("info").textContent = ("Rock beats scissors!");
+        return document.getElementById("player-score").textContent = `Player: ${++playerWinCounter}`;
     }
     else if (computerSelection == "scissors" && playerSelection == "paper") {
-        document.getElementById("info").textContent = ("You lose!");
-        return document.getElementById("computer-score").textContent = `Computer: ${++computerWin}`;
+        document.getElementById("info").textContent = ("Paper lose to scissors!");
+        return document.getElementById("computer-score").textContent = `Computer: ${++computerWinCounter}`;
     }
     else if (computerSelection == playerSelection) {
         return document.getElementById("info").textContent = ("Tie!");
@@ -58,27 +79,22 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
 
-    document.getElementById("rock").disabled = false;
-    document.getElementById("paper").disabled = false;
-    document.getElementById("scissors").disabled = false;
-    
-    choices.forEach((choice) => choice.addEventListener("click", () => { playRound(choice.id, computerSelection)
-    
-        if (playerWin == 5) {
-            document.getElementById("rock").disabled = true;
-            document.getElementById("paper").disabled = true;
-            document.getElementById("scissors").disabled = true;
+    enableControls();
+
+    choices.forEach((choice) => choice.addEventListener("click", () => {
+        playRound(choice.id)
+
+        if (playerWinCounter == 5) {
+            disableControls();
 
             return document.getElementById("info").textContent = ("YOU WON!");
         }
-        else if (computerWin == 5) {
-            document.getElementById("rock").disabled = true;
-            document.getElementById("paper").disabled = true;
-            document.getElementById("scissors").disabled = true;
+        else if (computerWinCounter == 5) {
+            disableControls();
 
             return document.getElementById("info").textContent = ("YOU LOST!")
         };
-    
+
     }));
 
 };
